32c40decb0429ef0071455faacfb3be44cf65941
Lucky B Wavai <39504510+lbw798@users.noreply.github.com>
2024-07-18 09:38:46 -0400

    Create move_base.ino

---
+++ move_base.ino
@@ -0,0 +1,75 @@
+/*
+Move Base Template Code from Sunfounder Documentation
+
+https://docs.sunfounder.com/projects/3in1-kit-v2/en/latest/car_project/car_move_by_code.html
+*/
+
+const int A_1B = 5;
+const int A_1A = 6;
+const int B_1B = 9;
+const int B_1A = 10;
+
+void setup() {
+  pinMode(A_1B, OUTPUT);
+  pinMode(A_1A, OUTPUT);
+  pinMode(B_1B, OUTPUT);
+  pinMode(B_1A, OUTPUT);
+}
+
+// Example Move Sequence
+void loop() {
+  moveForward();
+  delay(2000);
+  stopMove();
+  delay(500);
+
+  moveBackward();
+  delay(2000);
+  stopMove();
+  delay(500);
+
+  turnLeft();
+  delay(2000);
+  stopMove();
+  delay(500);
+
+  turnRight();
+  delay(2000);
+  stopMove();
+  delay(500);
+}
+
+void moveForward() {
+  digitalWrite(A_1B, LOW);
+  digitalWrite(A_1A, HIGH);
+  digitalWrite(B_1B, HIGH);
+  digitalWrite(B_1A, LOW);
+}
+
+void moveBackward() {
+  digitalWrite(A_1B, HIGH);
+  digitalWrite(A_1A, LOW);
+  digitalWrite(B_1B, LOW);
+  digitalWrite(B_1A, HIGH);
+}
+
+void turnRight() {
+  digitalWrite(A_1B, HIGH);
+  digitalWrite(A_1A, LOW);
+  digitalWrite(B_1B, HIGH);
+  digitalWrite(B_1A, LOW);
+}
+
+void turnLeft() {
+  digitalWrite(A_1B, LOW);
+  digitalWrite(A_1A, HIGH);
+  digitalWrite(B_1B, LOW);
+  digitalWrite(B_1A, HIGH);
+}
+
+void stopMove() {
+  digitalWrite(A_1B, LOW);
+  digitalWrite(A_1A, LOW);
+  digitalWrite(B_1B, LOW);
+  digitalWrite(B_1A, LOW);
+}
